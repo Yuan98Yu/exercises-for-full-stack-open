d506e3f08c06caf5428bbd4e9145b43330005fda
Yuan98Yu <951080662@qq.com>
2019-10-16 12:40:08 +0800

    finish exercise1.12

--- part1/src/index.js
+++ part1/src/index.js
@@ -1,91 +1,37 @@
 import React, { useState } from 'react'
 import ReactDOM from 'react-dom'
 
-const Title = ({text}) => {
+const Button = ({text, handleClick}) => {
   return (
-    <h1>{text}</h1>
-  )
-}
-
-const Button = ({text, clickHandler}) => {
-  return (
-    <button onClick={clickHandler}>
+    <button onClick={handleClick}>
       {text}
     </button>
   )
 }
 
-const Buttons = ({buttonList}) => {
+const App = ({anecdotes}) => {
+  const [selected, setSelected] = useState(0)
+
   return (
-    <>
-      <Title text='give feedback' />
-      {
-        buttonList.map( element =>
-          <Button key={element.text} text={element.text} clickHandler={element.clickHandler} />
-        )
-      }
-    </>
+    <div>
+      <p>{anecdotes[selected]}</p>
+      <Button text="next anecdote" 
+        handleClick={() => setSelected( Math.floor(Math.random()*anecdotes.length) )} />
+    </div>
   )
 }
 
-const Statistics = ({numList}) => {
-  return (
-    <>
-      <Title text='statistics' />
-      <table>
-        <tbody>
-        {
-          numList.map(element => 
-            <tr key={element.text} > 
-              <td>{element.text}</td>
-              <td>{element.num}</td>
-            </tr>
-          )
-        }
-      </tbody>
-      </table>
-    </>
-  )
-}
-
-const App = () => {
-  // save clicks of each button to own state
-  const [good, setGood] = useState(0)
-  const [neutral, setNeutral] = useState(0)
-  const [bad, setBad] = useState(0)
-  const increaseNum = (num, setNum) => () => setNum(num + 1) 
-
-  let buttonList = [
-    {text:'good', clickHandler:increaseNum(good, setGood) },
-    {text:'neutral', clickHandler:increaseNum(neutral, setNeutral) },
-    {text:'bad', clickHandler:increaseNum(bad, setBad) },
-  ]
-  let statisticList = [
-    {text:'good', num:good},
-    {text:'neutral', num:neutral},
-    {text: 'bad', num:bad},
-    {text:'all', num:good+neutral+bad},
-    {text:'average', num:(good-bad) / (good+neutral+bad)},
-    {text:'positive', num: good / (good+neutral+bad)}
-  ]
-
-  let statistics = good+neutral+bad === 0 ? 
-    <p>"No feedback given"</p> : 
-    <Statistics numList={statisticList} />
+const anecdotes = [
+  'If it hurts, do it more often',
+  'Adding manpower to a late software project makes it later!',
+  'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+  'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+  'Premature optimization is the root of all evil.',
+  'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.'
+]
 
 
-  return (
-    <>
-      <div>
-        <Buttons buttonList={buttonList} />
-      </div>
-      <div>        
-        {statistics}
-      </div>
-    </>
-  )
-}
-
-ReactDOM.render(<App />, 
+ReactDOM.render(
+  <App anecdotes={anecdotes} />,
   document.getElementById('root')
 )
