89ed24bacdc4177f0830d9f0e84beb9c395cef0b
Yuan98Yu <951080662@qq.com>
2019-11-08 22:37:15 +0800

    finish exercise3.13, 3.14: Phonebook database step1 ~ step2

--- part3/index.js
+++ part3/index.js
@@ -1,3 +1,4 @@
+require('dotenv').config()
 const express = require('express')
 const app = express()
 const cors = require("cors")
@@ -6,98 +7,79 @@ const morgan = require('morgan')
 app.use(cors())
 app.use(express.static('build'))
 app.use(bodyParser.json())
-morgan.token('body', 
-	req => req.body?JSON.stringify(req.body):null)
+morgan.token('body',
+	req => req.body ? JSON.stringify(req.body) : null)
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 
-let persons = [
-	{
-		"name": "Arto Hellas",
-		"number": "040-123456",
-		"id": 1
-	},
-	{
-		"name": "Ada Lovelace",
-		"number": "39-44-5323523",
-		"id": 2
-	},
-	{
-		"name": "Dan Abramov",
-		"number": "12-43-234345",
-		"id": 3
-	},
-	{
-		"name": "Mary Poppendieck",
-		"number": "39-23-6423122",
-		"id": 4
-	}
-]
+const Person = require('./models/person')
 
 app.get('/', (req, res) => {
 	res.send('<h1>Hello World!</h1>')
 })
 
 app.get('/api/persons', (req, res) => {
-	res.json(persons)
+	Person
+		.find({})
+		.then(persons => {
+			console.log('phonebook:')
+			persons = persons.map(person => person.toJSON())
+			persons.forEach(person => console.log(`${person.name} ${person.number}`))
+			res.json(persons)
+		})
+		.catch(error => console.log(error))
 })
 
 app.get('/info', (req, res) => {
-	const count = persons.length
-	const date = new Date().toUTCString()
-	res.send(`<p>Phonebook has info for ${count} people</p><p>${date}</>`)
+	Person
+		.find({})
+		.then(persons => {
+			const count = persons.length
+			const date = new Date().toUTCString()
+			res.send(`<p>Phonebook has info for ${count} people</p><p>${date}</>`)
+		})
+		.catch(error => console.log(error))
 })
 
 app.get('/api/persons/:id', (request, response) => {
-	const id = Number(request.params.id)
-	const person = persons.find(person => person.id === id)
+	const id = request.params.id
 
-	if (person) {
-		response.json(person)
-	} else {
-		response.status(404).end()
-	}
-})
-
-app.delete('/api/persons/:id', (request, response) => {
-	const id = Number(request.params.id)
-	person = persons.find(person => person.id === id)
-	console.log(person?`found person:${person}\ndel...`:`not found id:${id}`)
-	persons = persons.filter(person => person.id !== id)
-
-	response.status(204).end()
+	Person
+		.find({ _id: id })
+		.then(persons => {
+			const person = persons[0].toJSON()
+			response.json(person)
+		})
+		.catch(() => {
+			response.status(404).end()
+		})
 })
 
 app.post('/api/persons', (request, response) => {
-	const max = 65535
-	const generateId = () => Math.floor(Math.random() * max)
-
 	const body = request.body
+	// if (!body.name || !body.number) {
+	// 	return response.status(400).json({
+	// 		error: 'name or number missing'
+	// 	})
+	// }
+	// if (persons.map(person => person.name).includes(body.name)) {
+	// 	return response.status(400).json({
+	// 		error: 'name must be unique'
+	// 	})
+	// }
 
-	if (!body.name || !body.number) {
-		return response.status(400).json({
-			error: 'name or number missing'
-		})
-	}
-	if (persons.map(person => person.name).includes(body.name)) {
-		return response.status(400).json({
-			error: 'name must be unique'
-		})
-	}
-
-	const person = {
+	const person = new Person({
 		name: body.name,
 		number: body.number,
-		id: generateId(),
-	}
+	})
 
-	persons = persons.concat(person)
-
-	response.json(person)
+	person.save().then(savedPerson => {
+		response.json(savedPerson.toJSON())
+	})
 })
 
 const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
+	console.log(`Server running on port ${PORT}`)
 })
 
 const unknownEndpoint = (request, response) => {
