76f595ce1f5e0031b6ae5ebc664819741b6b4257
Yuan98Yu <951080662@qq.com>
2019-11-10 13:10:43 +0800

    finish exercise4.1, 4.2: Blog list, step1, step2

---
+++ part4/app.js
@@ -0,0 +1,29 @@
+const config = require('./utils/config')
+const mongoose = require('mongoose')
+const express = require('express')
+const app = express()
+const bodyParser = require('body-parser')
+const cors = require('cors')
+const blogsRouter = require('./controllers/blogs')
+const middleware = require('./utils/middleware')
+
+console.log('connecting to', config.MONGODB_URI)
+mongoose
+	.connect(config.MONGODB_URI, { useNewUrlParser: true })
+	.then( () => {
+		console.log('connected to MongoDB')
+	})
+	.catch((error) => {
+		console.log('error connecting to MongoDB:', error.message)
+	})
+
+app.use(cors())
+app.use(bodyParser.json())
+app.use(middleware.requestLogger)
+
+app.use('/api/blogs', blogsRouter)
+
+app.use(middleware.unknownEndpoint)
+app.use(middleware.errorHandler)
+
+module.exports = app
---
+++ part4/controllers/blogs.js
@@ -0,0 +1,34 @@
+const blogsRouter = require('express').Router()
+const Blog = require('../models/blog')
+
+blogsRouter.get('/', (request, response, next) => {
+	Blog
+		.find({})
+		.then(blogs => blogs.map(blog => blog.toJSON()))
+		.then(blogs => {
+			response.json(blogs)
+		})
+		.catch(error => next(error))
+})
+
+blogsRouter.post('/', (request, response, next) => {
+	const blog = new Blog(request.body)
+
+	blog
+		.save()
+		.then(result => {
+			response.status(201).json(result)
+		})
+		.catch(error => next(error))
+})
+
+blogsRouter.delete('/:id', (request, response, next) => {
+	Blog
+		.findByIdAndRemove(request.params.id)
+		.then(() => {
+			response.status(204).end()
+		})
+		.catch(error => next(error))
+})
+
+module.exports = blogsRouter
